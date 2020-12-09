3e7ac83bd4fe0535e4b70102d6c7de6e5a630b96
moriel569 <moriel569@gmail.com>
2020-12-10 00:43:29 +0300

    Add gigs search

--- routes/gigs.js
+++ routes/gigs.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const db = require("../config/database");
 const Gig = require("../models/Gig");
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 
 // Get gig list
 router.get("/", (req, res) =>
@@ -9,7 +11,7 @@ router.get("/", (req, res) =>
     .then((gigs) => {
       console.log(gigs);
 
-      // New required sequre method
+      // New required secure method
       const context = {
         gigs: gigs.map((gig) => {
           return {
@@ -33,26 +35,80 @@ router.get("/add", (req, res) => res.render("add"));
 
 // Add gig
 router.post("/add", (req, res) => {
-  const data = {
-    title: "Wordpress Site",
-    technologies: "Wordpress, php , html, css",
-    budget: "$1000",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi gravida egestas quam sed bibendum. Nulla hendrerit mauris in tristique tristique. Quisque ut condimentum quam, a pharetra felis. Etiam eget ultricies lorem. In scelerisque velit nisi, eget tincidunt felis semper nec. Quisque ac lectus suscipit, pharetra enim sed, dictum orci. Sed eu ullamcorper est. Aenean gravida sodales ligula. Nam mollis arcu est. Curabitur ut molestie nisi.",
-    contact_email: "trav@gmail.com",
-  };
+  let { title, technologies, budget, description, contact_email } = req.body;
+  let errors = [];
 
-  let { title, technologies, budget, description, contact_email } = data;
+  // Validate fields
+  if (!technologies) {
+    errors.push({ text: "Please add technologies" });
+  }
+  if (!description) {
+    errors.push({ text: "Please add description" });
+  }
+  if (!title) {
+    errors.push({ text: "Please add title" });
+  }
+  if (!contact_email) {
+    errors.push({ text: "Please add contact email" });
+  }
 
-  // Insert into table
-  Gig.create({
-    title,
-    technologies,
-    budget,
-    description,
-    contact_email,
-  })
-    .then((gig) => res.redirect("/gigs"))
+  // Check for errors
+
+  if (errors.length > 0) {
+    res.render("add", {
+      errors,
+      title,
+      technologies,
+      budget,
+      description,
+      contact_email,
+    });
+  } else {
+    if (!budget) {
+      budget = "Unknown";
+    } else {
+      budget = `$${budget}`;
+    }
+
+    // Make lowercase and remove spaces
+    technologies = technologies.toLowerCase().replace(/, /g, ",");
+
+    // Insert data into table
+    Gig.create({
+      title,
+      technologies,
+      budget,
+      description,
+      contact_email,
+    })
+      .then((gig) => res.redirect("/gigs"))
+      .catch((err) => console.log(err));
+  }
+});
+
+// Search for gigs
+router.get("/search", (req, res) => {
+  let { term } = req.query;
+
+  // Make all case valid
+  term = term.toLowerCase();
+
+  Gig.findAll({ where: { technologies: { [Op.like]: "%" + term + "%" } } })
+    .then((gigs) => {
+      const context = {
+        gigs: gigs.map((gig) => {
+          return {
+            title: gig.title,
+            technologies: gig.technologies,
+            budget: gig.budget,
+            description: gig.description,
+            contact_email: gig.contact_email,
+          };
+        }),
+      };
+
+      res.render("gigs", { gigs: context.gigs });
+    })
     .catch((err) => console.log(err));
 });
 
